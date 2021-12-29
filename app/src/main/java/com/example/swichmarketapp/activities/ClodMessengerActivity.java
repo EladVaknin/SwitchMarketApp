4b0a7e72f9301c2c348960243490ea24e2c81c24
EladVaknin <74238558+EladVaknin@users.noreply.github.com>
2021-12-29 13:10:37 +0200

    finish cloud messenger

--- app/src/main/java/com/example/swichmarketapp/activities/ClodMessengerActivity.java
+++ app/src/main/java/com/example/swichmarketapp/activities/ClodMessengerActivity.java
@@ -19,25 +19,36 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.swichmarketapp.R;
-import com.example.swichmarketapp.adapter.ItemRecyclerAdapter;
-import com.example.swichmarketapp.models.Item;
+import com.example.swichmarketapp.adapter.UserAdapter;
+import com.example.swichmarketapp.utlities.CacheUtilities;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class ClodMessengerActivity extends AppCompatActivity implements ItemRecyclerAdapter.ItemClickListener {
+public class ClodMessengerActivity extends AppCompatActivity implements UserAdapter.ItemClickListener {
     private static final String TAG = "SearchActivity";
+    private final DatabaseReference mDbUser = FirebaseDatabase.getInstance().getReference(USERS_TABLE);
     private EditText mSearchEditText;
     private Button mSearchButton;
     private ProgressBar mProgressBar;
-    private final DatabaseReference mDbUser = FirebaseDatabase.getInstance().getReference(USERS_TABLE);
-    private ItemRecyclerAdapter mAdapter;
+    final List<String> allUsers = new ArrayList<>();
+    private HashMap<String, String> userPhotos = new HashMap<>();
+    private final FirebaseFirestore mDb = FirebaseFirestore.getInstance();
+
+
+    private UserAdapter mAdapter;
     private RecyclerView mRecyclerView;
 
 
@@ -46,6 +57,65 @@ public class ClodMessengerActivity extends AppCompatActivity implements ItemRecy
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloudmessanger);
         initViews();
+        fetchData();
+    }
+
+    private void fetchData() {
+        mDbUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot user : dataSnapshot.getChildren()) {
+                    if (user.child("profileUrl").exists()) {
+                        userPhotos.put((String) user.child("userName").getValue(), (String) user.child("profileUrl").getValue());
+                    }
+
+                }
+                String clientUserName = CacheUtilities.getUserName(ClodMessengerActivity.this);
+                mDb.collection("Chat").
+
+                        whereEqualTo(MessengerActivity.KEY_SEND_TO, clientUserName).
+
+                        get().
+
+                        addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        String otherUser = (String) document.getData().get(MessengerActivity.KEY_FROM_MSG);
+                                        if (!allUsers.contains(otherUser)) {
+                                            allUsers.add(otherUser);
+                                        }
+                                    }
+                                    mDb.collection("Chat").whereEqualTo(MessengerActivity.KEY_FROM_MSG, clientUserName).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                                    String otherUser = (String) document.getData().get(MessengerActivity.KEY_SEND_TO);
+                                                    if (!allUsers.contains(otherUser)) {
+                                                        allUsers.add(otherUser);
+                                                    }
+                                                }
+                                                mAdapter.setNewItems(allUsers, userPhotos);
+                                                recyclerViewShow(true);
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        });
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
     }
 
 
@@ -56,9 +126,10 @@ public class ClodMessengerActivity extends AppCompatActivity implements ItemRecy
         mSearchButton.setOnClickListener(v -> performSearch());
         mRecyclerView = findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new ItemRecyclerAdapter(this);
+        mAdapter = new UserAdapter(this);
         mAdapter.setClickListener(this);
         mRecyclerView.setAdapter(mAdapter);
+        recyclerViewShow(false);
     }
 
     private void recyclerViewShow(boolean shouldShow) {
@@ -71,121 +142,72 @@ public class ClodMessengerActivity extends AppCompatActivity implements ItemRecy
     private void performSearch() {
         recyclerViewShow(false);
         final String searchString = mSearchEditText.getText().toString();
-        mDbUser.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                final List<Item> itemList = new ArrayList<>();
-                for (DataSnapshot user : dataSnapshot.getChildren()) {
-                    if (user.child("items").exists()) {
-                        for (DataSnapshot item : user.child("items").getChildren()) {
-                            String description = (String) item.child("desc").getValue();
-                            Log.d(TAG, "Description -" + description);
-                            if (description.contains(searchString)) {
-                                String price = (String) item.child("price").getValue();
-                                String imageUrl = (String) item.child("imageItem").getValue();
-                                String toSwitch = (String) item.child("toSwitch").getValue();
-                                String userName = (String) user.child("userName").getValue();
-                                itemList.add(new Item(description, imageUrl, toSwitch, price, userName));
-                            }
-                        }
-
-                    }
-                }
-                mAdapter.setNewItems(itemList);
-                recyclerViewShow(true);
+        ArrayList<String> filterArray = new ArrayList<>();
+        for (String str : allUsers) {
+            if (str.contains(searchString)) {
+                filterArray.add(str);
             }
+        }
+        mAdapter.setNewItems(filterArray,userPhotos);
+        recyclerViewShow(true);
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                mAdapter.clearAllData();
-                recyclerViewShow(true);
-            }
-        });
     }
 
     @Override
-    public void onItemClick(Item item) {
+    public void onUserClicked(String userName) {
         Intent intent = new Intent(ClodMessengerActivity.this, MessengerActivity.class);
-        intent.putExtra(MessengerActivity.SEND_TO_KEY, item.getUser());
+        intent.putExtra(MessengerActivity.SEND_TO_KEY, userName);
         startActivity(intent);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     // navigation menu items
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id=item.getItemId();
-        if(id==R.id.nav_mainpage)
-        {
-            Intent intent=new Intent(this, MainActivity.class);
+        int id = item.getItemId();
+        if (id == R.id.nav_mainpage) {
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
             return true;
         }
-        if(id==R.id.nav_messanger)
-        {
-            Intent intent=new Intent(this, ClodMessengerActivity.class);
+        if (id == R.id.nav_messanger) {
+            Intent intent = new Intent(this, ClodMessengerActivity.class);
             startActivity(intent);
             finish();
             return true;
         }
-        if(id==R.id.nav_profile)
-        {
-            Intent intent=new Intent(this, ProfileActivity.class);
+        if (id == R.id.nav_profile) {
+            Intent intent = new Intent(this, ProfileActivity.class);
             startActivity(intent);
             finish();
             return true;
         }
-        if(id==R.id.nav_search)
-        {
-            Intent intent=new Intent(this, SearchActivity.class);
+        if (id == R.id.nav_search) {
+            Intent intent = new Intent(this, SearchActivity.class);
             startActivity(intent);
             finish();
             return true;
         }
-        if(id==R.id.nav_toswitch)
-        {
-            Intent intent=new Intent(this, SwitchActivity.class);
+        if (id == R.id.nav_toswitch) {
+            Intent intent = new Intent(this, SwitchActivity.class);
             startActivity(intent);
             finish();
             return true;
         }
-        if(id==R.id.nav_aboutas)
-        {
-            Intent intent=new Intent(this, AboutAsActivity.class);
+        if (id == R.id.nav_aboutas) {
+            Intent intent = new Intent(this, AboutAsActivity.class);
             startActivity(intent);
             finish();
             return true;
         }
-        if(id==R.id.nav_logout)
-        {
+        if (id == R.id.nav_logout) {
             FirebaseAuth.getInstance().signOut();
             startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             finish();
---
+++ app/src/main/java/com/example/swichmarketapp/adapter/UserAdapter.java
@@ -0,0 +1,97 @@
+package com.example.swichmarketapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.swichmarketapp.R;
+import com.example.swichmarketapp.models.Item;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ItemViewHolder> {
+
+    private final List<String> mData = new ArrayList<>();
+    private final HashMap<String,String> mPhotoUrlsHashMap= new HashMap();
+    private final LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public UserAdapter(Context context) {
+        this.mInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.user_row, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        String userName = mData.get(position);
+        holder.mUserName.setText(userName);
+        if(mPhotoUrlsHashMap.containsKey(userName)){
+            Picasso.get().load(mPhotoUrlsHashMap.get(userName)).noPlaceholder().into(holder.mPhoto);
+        }
+
+    }
+
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void clearAllData() {
+        mData.clear();
+        notifyDataSetChanged();
+
+    }
+
+    public void setNewItems(List<String> itemList, HashMap<String,String> profileUrls) {
+        mPhotoUrlsHashMap.clear();
+        mPhotoUrlsHashMap.putAll(profileUrls);
+        mData.clear();
+        mData.addAll(itemList);
+        notifyDataSetChanged();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
+        TextView mUserName;
+        ImageView mPhoto;
+
+        ItemViewHolder(View itemView) {
+            super(itemView);
+            mPhoto = itemView.findViewById(R.id.photoUrl);
+            mUserName = itemView.findViewById(R.id.userNameTextView);
+            mUserName.setOnClickListener(v -> mClickListener.onUserClicked(mData.get(getAdapterPosition())));
+        }
+
+
+    }
+
+
+    // allows clicks events to be caught
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onUserClicked(String userName);
+    }
+}
